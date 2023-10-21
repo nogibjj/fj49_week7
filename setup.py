a4b1768186cfc6bb6cb2af89b55f5b4303155ea2
farazjawedd <101464414+farazjawedd@users.noreply.github.com>
2023-10-21 22:30:08 +0000

    packaged

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 from setuptools import setup
 
 setup(
-    name="easyfast-csv-splitter",
+    name="my-csv-splitter",
     version="1.0.0",
-    author="Faraz Jawed",
+    author="Your Name",
     description="A tool to split CSV files",
-    packages=["my_csv_splitter"],
+    py_modules=["main"],
+    install_requires=[
+        'click',
+    ],
     entry_points={
         "console_scripts": [
-            "csv-splitter = my_csv_splitter.my_csv_splitter:main",
+            "csv-splitter = main:split_csv",
         ],
     },
 )
+
+
+# [pypi]
+#   username = __token__
+#   password = pypi-AgEIcHlwaS5vcmcCJDkxMzFmNjc0LTA5OWUtNGJmYy1iZWE2LWZkY2U1YWQzNGIzYgACKlszLCIzOWI1NmYxYy03NmI5LTQ2YmUtOTJjNC1jNjQ5OWM1MjA4YTEiXQAABiCZBTz5bc8v94fnohezHMcFmH8n8en4GIoLr62QoUMySA
