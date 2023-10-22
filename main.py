bea7cd2db6354cd2136203493900f6891975a773
farazjawedd <101464414+farazjawedd@users.noreply.github.com>
2023-10-22 01:07:46 +0000

    added test file

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
 import click
 import csv
 
-@click.command()
-@click.argument('input_file', type=click.Path(exists=True))
-@click.argument('output_prefix', type=click.STRING)
-@click.argument('chunk_size', type=click.INT)
+# Define the core functionality of your script without the @click.command() decorator
 def split_csv(input_file, output_prefix, chunk_size):
-    """
-    A tool to split a CSV file into smaller chunks.
-
-    INPUT_FILE: Path to the input CSV file.
-    OUTPUT_PREFIX: Prefix for the output files.
-    CHUNK_SIZE: Number of rows per output file (default: 100).
-    """
+    # Your core functionality here
     with open(input_file, 'r') as infile:
         reader = csv.reader(infile)
         header = next(reader)  # Assuming the first row is the header
@@ -35,10 +26,13 @@ def split_csv(input_file, output_prefix, chunk_size):
             writer.writerow(header)
         writer.writerow(row)
 
+# Define the command-line interface separately
+@click.command()
+@click.argument('input_file', type=click.Path(exists=True))
+@click.argument('output_prefix', type=click.STRING)
+@click.argument('chunk_size', type=click.INT)
+def main(input_file, output_prefix, chunk_size):
+    split_csv(input_file, output_prefix, chunk_size)
+
 if __name__ == '__main__':
-    split_csv()
-
-
-
-
-
+    main()
--- test_main.py
+++ test_main.py
@@ -1,10 +1,20 @@
-"""
-Test goes here
+import os
+import pytest
+from main import split_csv
 
-"""
+def test_split_csv():
+    input_file = 'spotify.csv'  # Replace with the actual file path
+    output_prefix = 'output_chunk'
+    chunk_size = 1000  # Provide as an integer
 
-from mylib.calculator import add
+    
 
+    split_csv(input_file, output_prefix, chunk_size)
 
-def test_add():
-    assert add(1, 2) == 3
+    # Check if the expected output files are generated
+    for i in range(1, 4):  # Assuming it will create 3 output files for the test case
+        output_file = f'{output_prefix}_{i}.csv'
+        assert os.path.exists(output_file)
+
+if __name__ == '__main__':
+    test_split_csv()
